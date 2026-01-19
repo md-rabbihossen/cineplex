5b42ca0c73bdd47b1a9dcfb7fbc5d2cbd3dfd02a
mdrabbihossen <rabbihossenrabbi24@gmail.com>
2026-01-19 16:15:07 +0600

    Added Movie Lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./components/Header";
+import MovieLists from "./components/Movies/MovieLists";
 import Sidebar from "./components/Sidebar";
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
         <Header />
         <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
           <Sidebar />
+          <MovieLists />
         </div>
       </div>
     </>
