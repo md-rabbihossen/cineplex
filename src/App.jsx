240f9aef70274246473e0439dee7a85554356748
mdrabbihossen <rabbihossenrabbi24@gmail.com>
2026-01-21 23:09:26 +0600

    Footer Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MovieLists from "./components/Movies/MovieLists";
 import Sidebar from "./components/Sidebar";
 
+
 export default function App() {
   return (
     <>
-      <div className="m-3">
-        <Header />
-        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
-          <Sidebar />
-          <MovieLists />
+      {" "}
+      <main>
+        <div className="m-3">
+          <Header />
+          <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+            <Sidebar />
+            <MovieLists />
+          </div>
         </div>
-      </div>
+      </main>
+      <Footer />
     </>
   );
 }
